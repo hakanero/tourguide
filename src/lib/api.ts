9cd4db688a88ecbd0afcf975d8afa25e2614786f
Hakan <hakan@mekbuk.local>
2025-10-05 03:20:52 -0400

    fix api

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -2,7 +2,7 @@ import { reverseGeocode } from "./utils";
 
 export async function getData(coords: { lat: number; lng: number }, language: string){
 	// Base backend URL (no trailing slash to make concat predictable)
-	const apilink = "";//"https://theroamerbackend.onrender.com/audio";
+	const apilink = "https://theroamerbackend.onrender.com/audio";
 
 	const imageUrl =
 		"https://www.tclf.org/sites/default/files/thumbnails/image/HarvardUniversity-sig.jpg";
